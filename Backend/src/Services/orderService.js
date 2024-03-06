df8071d7c42b356fea9249152b20100ee6105ff8
prajval_100 <prajwalghusalikar@gmail.com>
2024-03-06 23:54:54 +0530

    Add: createOrder in orderService.js

---
+++ Backend/src/Services/orderService.js
@@ -0,0 +1,47 @@
+const Address = require("../Models/addressModel");
+const cartService = require("./cartService");
+
+const createOrder = async (user, shippingAddress) => {
+  let address;
+  if (shippingAddress._id) {
+    let existAddress = await Address.findById(shippingAddress._id);
+    address = existAddress;
+  } else {
+    address = new Address(shippingAddress);
+    address.user = user;
+    await address.save();
+
+    user.addresses.push(address);
+    await user.save();
+  }
+
+  const cart = await cartService.findUserCart(user._id);
+  const orderItems = [];
+
+  for (const item of cart.cartItems) {
+    const orderItems = new orderItems({
+      price: item.price,
+      product: item.product,
+      quantity: item.quantity,
+      size: item.size,
+      userId: item.userId,
+      discountedPrice: item.discountedPrice,
+    });
+
+    const createdOrderItem = await orderItem.save();
+    orderItems.push(createdOrderItem);
+  }
+
+  const createdOrder = new Order({
+    user,
+    orderItems,
+    totalPrice: cart.totalPrice,
+    totalDiscountedPrice: cart.totalDiscountedPrice,
+    discount: cart.discount,
+    totalItem: cart.totalItems,
+    shippingAddress: address,
+  });
+
+  const savedOrder = await createOrder.save();
+  return savedOrder;
+};
