8bb6f6831cc1b4a3ae6e0cc577e1fd31e33502ba
prajval_100 <prajwalghusalikar@gmail.com>
2024-03-21 23:59:09 +0530

    Add: customerProductReducer in productReducer.js and export in store

---
+++ Frontend/src/Store/Products/ProductReducer.js
@@ -0,0 +1,56 @@
+import {
+  FIND_PRODUCTS_FAILURE,
+  FIND_PRODUCTS_REQUEST,
+  FIND_PRODUCTS_SUCCESS,
+  FIND_PRODUCT_BY_ID_FAILURE,
+  FIND_PRODUCT_BY_ID_REQUEST,
+  FIND_PRODUCT_BY_ID_SUCCESS,
+} from "./AcrionType";
+
+const initialState = {
+  products: [],
+  product: null,
+  loading: false,
+  error: null,
+};
+
+export const customerProductReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FIND_PRODUCTS_REQUEST:
+    case FIND_PRODUCT_BY_ID_REQUEST:
+      return { ...state, loading: true, error: null };
+
+    case FIND_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        products: action.payload,
+      };
+
+    case FIND_PRODUCT_BY_ID_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        products: action.payload,
+      };
+
+    case FIND_PRODUCTS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    case FIND_PRODUCT_BY_ID_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
--- Frontend/src/Store/store.js
+++ Frontend/src/Store/store.js
@@ -1,10 +1,11 @@
-import {applyMiddleware, combineReducers, legacy_createStore} from "redux"
-import {thunk} from "redux-thunk"
-import { authReducers } from "./Auth/Reducer"
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { authReducers } from "./Auth/Reducer";
+import { customerProductReducer } from "./Products/ProductReducer";
 
 const rootReducers = combineReducers({
-  auth: authReducers
-})
+  auth: authReducers,
+  product: customerProductReducer,
+});
 
-export const store = legacy_createStore(rootReducers,applyMiddleware(thunk))
-
+export const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
